f911beb934d775984ed39233e5d08be89df8c297
hu <hupj@neusoft.com>
2023-11-14 09:58:51 +0800

    change from arrow to space

--- src/jpeg/include/jpeg.h
+++ src/jpeg/include/jpeg.h
@@ -1,13 +1,13 @@
 
 /*
- *	A declare for Jpeg
- *	Written by hopeking <hupingjin@163.com>
+ *  A declare for Jpeg
+ *  Written by hopeking <hupingjin@163.com>
  *
  *  Declare interface of Jpeg.
  *
- *	Copyright 2023 hopeking.
+ *  Copyright 2023 hopeking.
  *
- *	See ../../LICENSE  for licensing terms.
+ *  See ../../LICENSE  for licensing terms.
  */
 
 
