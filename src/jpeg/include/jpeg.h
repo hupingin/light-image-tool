10115fe16be84d758cd58f773b67f258599c4f24
hu <hupj@neusoft.com>
2023-11-14 09:40:40 +0800

    add declare for jpeg.

---
+++ src/jpeg/include/jpeg.h
@@ -0,0 +1,15 @@
+
+/*
+ *	A declare for Jpeg
+ *	Written by hopeking <hupingjin@163.com>
+ *
+ *  Declare interface of Jpeg.
+ *
+ *	Copyright 2023 hopeking.
+ *
+ *	See ../../LICENSE  for licensing terms.
+ */
+
+
+
+
